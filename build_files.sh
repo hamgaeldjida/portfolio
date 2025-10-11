bd56f058914fd516b6e8bcc71af80e7f0fba86ba
hamgaeldjida <hamgaeldjida@gmail.com>
2025-10-11 15:32:23 +0200

    change styles - 3

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "BUILD START"
 
 # create a virtual environment named 'venv' if it doesn't already exist
@@ -6,10 +8,15 @@ python3.9 -m venv venv
 # activate the virtual environment
 source venv/bin/activate
 
-# install all deps in the venv
+# install all dependencies in the venv
 pip install -r requirements.txt
 
+# delete old static files
+echo "Deleting old static files..."
+rm -rf staticfiles  # adjust path if needed (e.g., 'static' or 'app_name/static')
+
 # collect static files using the Python interpreter from venv
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "BUILD END"
